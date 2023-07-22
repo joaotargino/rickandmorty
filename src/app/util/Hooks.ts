de15fed3a01d1f5c810ae27733e735d8778bdeae
Joao P Targino <joaotargino@gmail.com>
2023-07-22 15:57:29 -0400

    package.json

--- src/app/util/Hooks.ts
+++ src/app/util/Hooks.ts
@@ -1,5 +1,5 @@
 import { useLayoutEffect, useState } from 'react';
-import debounce from 'lodash/debounce';
+// import debounce from 'lodash/debounce';
 
 const useIsMobile = (): boolean => {
   const [isMobile, setIsMobile] = useState(false);
@@ -8,7 +8,7 @@ const useIsMobile = (): boolean => {
     const updateSize = (): void => {
       setIsMobile(window.innerWidth < 768);
     };
-    window.addEventListener('resize', debounce(updateSize, 250));
+    // window.addEventListener('resize', debounce(updateSize, 250));
     // updateSize();
     return (): void => window.removeEventListener('resize', updateSize);
   }, []);
